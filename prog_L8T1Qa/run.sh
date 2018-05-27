58b889cd262b5f7fc85aa28d738de90a2335861f
Yann Chemin <ychemin@gmail.com>
2018-05-27 15:30:02 +0200

    update for GCP

--- prog_L8T1Qa/run.sh
+++ prog_L8T1Qa/run.sh
@@ -15,8 +15,8 @@ make
 # Number of running threads
 export OMP_NUM_THREADS=$1
 
-#Basename of satellite imagery tile with Path and Row
-productL8=LC08_L1TP_142054_
+#Basename of satellite imagery tile without Path and Row
+productL8=LC08_L1TP_
 
 #PWD program
 PWD=$(pwd)
@@ -34,25 +34,25 @@ rm $out_l8/*.tif -f
 
 #Process by timestamp range
 for (( doy = 2018000 ; doy <= 2019000 ; doy++ ))
-do 	test0=$(find $in_l8 -type f | grep $productL8$doy | grep tar.gz | wc -l)
+do 	test0=$(find $in_l8 -type f | grep $productL8 | grep $doy | grep tar.gz | wc -l)
 	if [ $test0 -eq 1 ] 
 	then 
 		# Uncompress tarball
-		tar xvf $(find $in_l8 -type f | grep $productL8$doy | grep tar.gz) -C $in_l8 
+		tar xvf $(find $in_l8 -type f | grep $productL8 | grep $doy | grep tar.gz) -C $in_l8 
 		# Get input files count
-		test1=$(find $in_l8 -type f | grep $productL8$doy | grep _B1.TIF | wc -l)
-		test2=$(find $in_l8_qa -type f | grep $productL8$doy | grep _BQA.TIF | wc -l)
+		test1=$(find $in_l8 -type f | grep $productL8 | grep $doy | grep _B1.TIF | wc -l)
+		test2=$(find $in_l8_qa -type f | grep $productL8 | grep $doy | grep _BQA.TIF | wc -l)
 		# Get output file count
-  		test3=$(find $out_l8 -type f | grep $out_l8$productL8$doy.tif | wc -l)
+  		test3=$(find $out_l8 -type f | grep $out_l8$productL8\_$doy.tif | wc -l)
 		#if output exists, do not overwrite (test3 -eq 0)
 		if [ $test1 -eq 1 -a $test2 -eq 1 -a $test3 -eq 0 ]
   		then 
 			# Get Band 1
-			inB2=$(find $in_l8 -type f | grep $productL8$doy | grep _B1.TIF)
+			inB2=$(find $in_l8 -type f | grep $productL8 | grep $doy | grep _B1.TIF)
 			# Get QA band
-			inB3=$(find $in_l8 -type f | grep $productL8$doy | grep _BQA.TIF)
+			inB3=$(find $in_l8 -type f | grep $productL8 | grep $doy | grep _BQA.TIF)
 			# Set output name
-			outL8=$out_l8$productL8$doy.tif
+			outL8=$out_l8$productL8\_$doy.tif
 			# Process
 			echo "./l8t1qa $inB2 $inB3 $outL8" 
 			./l8t1qa $inB2 $inB3 $outL8 
