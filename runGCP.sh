33c73d7c04dc4159c18edd0a97e3438acb325b5b
Yann Chemin <ychemin@gmail.com>
2018-05-27 17:23:33 +0200

    update

--- runGCP.sh
+++ runGCP.sh
@@ -11,17 +11,17 @@ echo "$3 is the local output reception directory"
 echo "Run GCP instance"
 gcloud beta compute --project=able-bazaar-205117 instances create instance-2 --zone=northamerica-northeast1-a --machine-type=n1-standard-$1 --subnet=default --network-tier=PREMIUM --metadata=startup-script=sudo\ apt-get\ update$'\n'sudo\ apt-get\ install\ subversion\ libgdal-dev\ -y$'\n'svn\ co\ https://github.com/YannChemin/L8T1Qa$'\n'cd\ L8T1Qa/trunk/prog_L8T1Qa/ --maintenance-policy=MIGRATE --service-account=904205699198-compute@developer.gserviceaccount.com --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append --image=ubuntu-1804-bionic-v20180522 --image-project=ubuntu-os-cloud --boot-disk-size=10GB --boot-disk-type=pd-standard --boot-disk-device-name=instance-2
 
-echo "Copy Data from local source (enable manually if needed)"
+echo "Copy Data from local source (enable/disable if needed)"
 #PRIOR TO PROCESSING: from local to gcp
 ##Go to directory from interactive user setting
-#cd $2
+cd $2
 ##Go to directory for Yann Local laptop
 ##cd ~/RSDATA/2_PreProcessed/L8/
 ##Upload each tarball with scp from directory set
-#for file in *.tar.gz
-#do
-#	gcloud compute scp $file instance-2:~/L8T1Qa/trunk/RSDATA/2_PreProcessed/L8/
-#done
+for file in *.tar.gz
+do
+	gcloud compute scp $file instance-2:~/L8T1Qa/trunk/RSDATA/2_PreProcessed/L8/
+done
 
 #SETUP
 gcloud compute ssh --zone=northamerica-northeast1-a instance-2 --command 'sudo apt-get update && sudo apt-get install subversion libgdal-dev && sudo apt autoremove'
