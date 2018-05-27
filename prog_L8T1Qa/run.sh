937bdb6f02a7ffee9d977fe626e694464a6335eb
Yann Chemin <ychemin@gmail.com>
2018-05-27 10:48:17 +0200

    Update

--- prog_L8T1Qa/run.sh
+++ prog_L8T1Qa/run.sh
@@ -25,39 +25,39 @@ in_l8_qa=$DataRoot/2_PreProcessed/L8/
 out_l8=$root/$2/
 
 #Make output directory
-mkdir $out_l8
+mkdir -p $out_l8
 rm $out_l8/*.tif -f
 
 #Process by timestamp range
 for (( doy = 2018000 ; doy <= 2019000 ; doy++ ))
 do 	test0=$(find $in_l8 -type f | grep $productL8$doy | grep tar.gz | wc -l)
+	echo $test0
 	if [ $test0 -eq 1 ] 
 	then 
 		tar xvf $test0
-		do 	test1=$(find $in_l8 -type f | grep $productL8$doy | wc -l)
-			test2=$(find $in_l8_qa -type f | grep $productL8$doy | wc -l)
-  			test3=$(find $out_l8 -type f | grep $out_l8$productL8$doy\_L8.tif | wc -l)
- 			#if output exists, do not overwrite
-			if [ $test1 -eq 1 -a $test2 -eq 1 -a $test3 -eq 0 ]
-  			then 
-				inB2=$in_l8$productL8$doy\_L8.tif 
-				inB3=$in_l8_qa$productL8$doy\_L8_QA.tif 
-				outL8=$out_l8$productL8$doy.tif 
-				# Process
-				echo "./l8T1Qa $inB2 $inB3 $outL8" 
-				./l8T1Qa $inB2 $inB3 $outL8 
-				# Clean up
-				rm -f $in_l8/*.tif
-				rm -f $in_l8/*.txt
-				# Tarball the output file & clean up
-				cd $out_l8
-				for file in *.tif
-				do	tar -cvzf $(echo $file| sed 's/.tif//').tar.gz $file
-					rm -f $file
-				done
-				# Return to program dir
-				cd $PWD
-			fi 
-		done
+		test1=$(find $in_l8 -type f | grep $productL8$doy | wc -l)
+		test2=$(find $in_l8_qa -type f | grep $productL8$doy | wc -l)
+  		test3=$(find $out_l8 -type f | grep $out_l8$productL8$doy\_L8.tif | wc -l)
+ 		#if output exists, do not overwrite
+		if [ $test1 -eq 1 -a $test2 -eq 1 -a $test3 -eq 0 ]
+  		then 
+			inB2=$in_l8$productL8$doy\_L8.tif 
+			inB3=$in_l8_qa$productL8$doy\_L8_QA.tif 
+			outL8=$out_l8$productL8$doy.tif 
+			# Process
+			echo "./l8T1Qa $inB2 $inB3 $outL8" 
+			./l8T1Qa $inB2 $inB3 $outL8 
+			# Clean up
+			rm -f $in_l8/*.tif
+			rm -f $in_l8/*.txt
+			# Tarball the output file & clean up
+			cd $out_l8
+			for file in *.tif
+			do	tar -cvzf $(echo $file| sed 's/.tif//').tar.gz $file
+				rm -f $file
+			done
+			# Return to program dir
+			cd $PWD
+		fi 
 	fi
 done
