1ff51f8ec436d1c77734a013aa15b9436b61d79f
Yann Chemin <ychemin@gmail.com>
2018-06-04 15:26:24 +0200

    Updated with python subprocess example script

--- prog_L8T1Qa/run.py
+++ prog_L8T1Qa/run.py
@@ -1,5 +1,9 @@
 #!/usr/env python
 
+# Argv[1] = L8 Band x
+# Argv[2] = L8 Qa Band
+# Argv[3] = L8 corrected Output Band x
+
 import sys
 import subprocess
 err=subprocess.call(['./l8t1qa',sys.argv[1],sys.argv[2],sys.argv[3]])
