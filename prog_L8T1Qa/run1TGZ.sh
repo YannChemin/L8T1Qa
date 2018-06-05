ca5da858a174645bc0cc0c5e8027b4487351c4b4
Yann Chemin <ychemin@gmail.com>
2018-06-05 19:40:45 +0200

    update

--- prog_L8T1Qa/run1TGZ.sh
+++ prog_L8T1Qa/run1TGZ.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+tgzF=$1
+
 if [ $# -lt 1 ]; then
   echo 1>&2 "$0: not enough arguments: TGZ input file"
   exit 2
@@ -12,25 +14,27 @@ fi
 in_l8=~/RSDATA/IN_L8/IN_L8_Ya
 
 #Make output directory
-out_l8=~/RSDATA/OUT_L8
-mkdir-p $out_l8
+out_l8=~/RSDATA/OUT_L8_Ya
+mkdir -p $out_l8
 rm $out_l8/*.tif -f
 
 # Recompile with latest changes
-make clean
-make
+make clean &>/dev/null
+make &>/dev/null
 
-#PWD program
-PWD=$(pwd)
+
+#pwd of program
+prog=~/dev/L8T1Qa/trunk/prog_L8T1Qa/
 
 #CD to input dir
 cd $in_l8
 
 # Uncompress tarball
-tar -I pigz -xvf $1 -C $(echo $1 | sed 's/.tar.gz//')
+mkdir -p $in_l8/$(echo $1 | sed 's/.tar.gz//')
+tar -I pigz -xvf $tgzF -C $in_l8/$(echo $1 | sed 's/.tar.gz//') &>/dev/null
 
 # Go to untarballed dir
-cd ~/RSDATA/IN_L8/$(echo $1 | sed 's/.tar.gz//')
+cd $in_l8/$(echo $tgzF | sed 's/.tar.gz//')
 
 # Set input names
 inB4=$(ls *band4.tif)
@@ -39,16 +43,16 @@ inB6=$(ls *band6.tif)
 inBQ=$(ls *pixel_qa.tif)
 
 # Set output names
-outL8VI=$out_l8$(echo $1 | sed 's/.tar.gz//')\_NDVI.tif
-outL8WI=$out_l8$(echo $1 | sed 's/.tar.gz//')\_NDWI.tif
+outL8VI=$out_l8/$(echo $tgzF | sed 's/.tar.gz//')\_NDVI.tif
+outL8WI=$out_l8/$(echo $tgzF | sed 's/.tar.gz//')\_NDWI.tif
 	
 # Set temp names
-tmpL8VI=$out_l8\_NDVI.tif
-tmpL8WI=$out_l8\_NDWI.tif
+tmpL8VI=$out_l8/temp_$(echo $tgzF | sed 's/.tar.gz//')\_NDVI.tif
+tmpL8WI=$out_l8/temp_$(echo $tgzF | sed 's/.tar.gz//')\_NDWI.tif
 	
 # Process
-echo "$PWD/l8t1qa $inB4 $inB5 $inB6 $inBQ $tmpL8VI $tmpL8WI" 
-$PWD/l8t1qa $inB4 $inB5 $inB6 $inBQ $tmpL8VI $tmpL8WI
+#echo "$prog/l8t1qa $inB4 $inB5 $inB6 $inBQ $tmpL8VI $tmpL8WI" 
+$prog/l8t1qa $inB4 $inB5 $inB6 $inBQ $tmpL8VI $tmpL8WI
 #echo "python ./run.py $inB4 $inB5 $inB6 $inBQ $tmpL8VI $tmpL8WI" 
 #python ./run.py $inB4 $inB5 $inB6 $inBQ $tmpL8VI $tmpL8WI 
 
@@ -58,14 +62,14 @@ rm -Rf $in_l8/$(echo $1 | sed 's/.tar.gz//')
 
 # Go to output Dir
 cd $out_l8
-echo "cd $out_l8"
+#echo "cd $out_l8"
 
 # Convert tmp to EPSG:4326
-gdalwarp -multi -t_srs "EPSG:4326" $tmpL8VI $outL8VI
-gdalwarp -multi -t_srs "EPSG:4326" $tmpL8WI $outL8WI
+gdalwarp -q -multi -t_srs "EPSG:4326" $tmpL8VI $outL8VI
+gdalwarp -q -multi -t_srs "EPSG:4326" $tmpL8WI $outL8WI
 
 # Tarball the output file & clean up
-tar -I pigz -cvzf $outL8VI.tar.gz $outL8VI 
-tar -I pigz -cvzf $outL8WI.tar.gz $outL8WI 
-rm -f $outL8VI $outL8WI
+tar -I pigz -cvf $(echo $outL8VI | sed 's/.tif//').tar.gz $outL8VI &>/dev/null
+tar -I pigz -cvf $(echo $outL8WI | sed 's/.tif//').tar.gz $outL8WI &>/dev/null
+rm -f temp*
 rm -f *.IMD
